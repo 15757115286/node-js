0b4013f24db384d735ce7b5585d5a04281a8e20c
xuweitao <2270532540@qq.com>
2018-10-09 17:10:19 +0800

    文件流测试

--- test.js
+++ test.js
@@ -19,3 +19,35 @@ function testReadable() {
     fs.writeFile("wl2.png", data, () => {});
   });
 }
+
+// 读取流刚创建的时候调用isPaused返回的是false，但是此时是没有数据生成的。
+// 这个时候readable._readableState.flowing = null，是初始化的状态。
+// 此时如果调用readable.pipe() || readable.on('data',d=>{...}) || readable.resume()，
+// 都会使得readable._readableState.flowing = true（添加data事件只有在readable._readableState.flowing = null的时候才能设置为true），
+// readable.pipe()可以显示的将readable._readableState.flowing = false 变成 true，同时也会触发data的监听事件
+// 前两种方式都会将产生的数据丢给“消费”的函数，只有第三种情况会把数据流失掉（如果没有监听data事件），
+// 一般也用readable.resume()去消耗不需要处理的数据。
+function testPasueAndResume(){
+    //let readable = fs.createReadStream("wl.jpg");
+    let readable = new fs.ReadStream('wl.txt');
+    readable.pause();
+    isPaused = readable.isPaused();
+    /* setTimeout(()=>{
+        let chunk = readable.read();
+        console.log(chunk.toString())
+    },1000); */
+    readable.on('data',d=>{
+        console.log(d.length)
+    })
+
+    readable.on('end',()=>{
+        console.log('end');
+    })
+    setTimeout(()=>{
+        console.log('start resume')
+        readable.resume();
+    },10000)
+    readable.pipe(new fs.createWriteStream('mywl.txt'));
+   // readable.resume();
+}   
+testPasueAndResume();
